ceb90d505989fbfe1732f0e63fdc1404c84ee1e9
Alon Budker <alonbudker@mail.tau.ac.il>
2023-02-23 20:04:01 +0200

    refactor: aligned models to black-style formatting

--- src/operank_scheduling/models/operating_room.py
+++ src/operank_scheduling/models/operating_room.py
@@ -1,11 +1,12 @@
 from typing import List
 from models.surgery import Surgery
 
-class OperatingRoom():
+
+class OperatingRoom:
     def __init__(self, id: str, properties: List[str]) -> None:
         self.id = id
         self.properties = properties
         self.surgeries_to_schedule: List[Surgery] = list()
-    
+
     def __repr__(self) -> str:
         return self.id
--- src/operank_scheduling/models/surgery.py
+++ src/operank_scheduling/models/surgery.py
@@ -1,10 +1,13 @@
 from typing import List
 
-class Surgery():
-    def __init__(self, name: str, duration_in_minutes: int, requirements: List[str]) -> None:
+
+class Surgery:
+    def __init__(
+        self, name: str, duration_in_minutes: int, requirements: List[str]
+    ) -> None:
         self.name = name
         self.duration = duration_in_minutes
         self.requirements = requirements
-    
+
     def __repr__(self) -> str:
-        return f"{self.name}({self.duration}m)"
+        return f"{self.name} ({self.duration}m)"
