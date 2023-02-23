dd79ab1582aa7887b091fd74fa5781021cca1122
Alon Budker <alonbudker@mail.tau.ac.il>
2023-02-23 20:05:48 +0200

    fix: rewrote model constraints to get better solve rates

--- src/operank_scheduling/algo/distribution_models.py
+++ src/operank_scheduling/algo/distribution_models.py
@@ -1,99 +1,125 @@
 from ortools.sat.python import cp_model
-import numpy as np
-from typing import Dict
+from typing import Dict, List, Any
 from math import factorial
+from loguru import logger
+
+from .algo_helpers import lazy_permute
+from models.operating_room import OperatingRoom
+from models.surgery import Surgery
 
 MAX_VAL_LIM = 10000
 
-def create_data_dict() -> Dict:
-    """Create the data for the example."""
-    data = dict()
-    operating_rooms = 3
-    surgery_durations = [120, 60, 60, 120, 180, 60]
-    data['avg_duration'] = int(np.mean(surgery_durations) * len(surgery_durations))
-    data['surgery_durations'] = surgery_durations
-    data['surgeries'] = list(range(len(surgery_durations)))
-    data['rooms'] = list(range(operating_rooms))
-    data['min_total_duration'] = min(surgery_durations) * len(surgery_durations) // operating_rooms
-    data['max_total_duration'] = max(surgery_durations) * len(surgery_durations) // operating_rooms
-    return data
+
+def restructure_data(
+    surgeries: List[Surgery], rooms: List[OperatingRoom]
+) -> Dict[str, Any]:
+    model_data = dict()
+    operating_rooms_amt = len(rooms)
+
+    model_data["surgery_durations"] = [surgery.duration for surgery in surgeries]
+    model_data["surgeries"] = list(range(len(model_data["surgery_durations"])))
+    model_data["rooms"] = list(range(operating_rooms_amt))
+
+    model_data["max_total_duration"] = (
+        max(model_data["surgery_durations"])
+        * len(model_data["surgery_durations"])
+        // operating_rooms_amt
+    )
+
+    return model_data
 
 
-def main():
-    data = create_data_dict()
-    # Create the mip solver with the SCIP backend.
+def distribute_surgeries_to_operating_rooms(
+    surgeries: List[Surgery], rooms: List[OperatingRoom]
+) -> Dict[OperatingRoom, List[Surgery]]:
+
+    data = restructure_data(surgeries, rooms)
     model = cp_model.CpModel()
 
     # Variables ---------------------------------------------
     # x[i, j] = 1 if surgery i is assigned to room j.
     x = {}
-    for i in data['surgeries']:
-        for j in data['rooms']:
-            x[(i, j)] = model.NewIntVar(0, 1, f'x_{i}_{j}')
+    for i in data["surgeries"]:
+        for j in data["rooms"]:
+            x[(i, j)] = model.NewIntVar(0, 1, f"x_{i}_{j}")
 
-    absolute_length_differences = [model.NewIntVar(0, MAX_VAL_LIM, f"diff_{i}") for i in range (factorial(len(data['rooms'])))]
+    absolute_length_differences = [
+        model.NewIntVar(0, MAX_VAL_LIM, f"diff_{i}")
+        for i in range(factorial(len(data["rooms"])))
+    ]
+    # End Variables -----------------------------------------
 
     # Constraints -------------------------------------------
-    # Each surgery must be in exactly one room. 
-    for surgery in data['surgeries']:
-        model.AddExactlyOne(x[surgery, j] for j in data['rooms'])
-    
-    # Each room should have at least one surgery.
-    # for j in data['rooms']:
-    #     model.Add(sum(x[i, j] for i in data['surgeries']) >= 1)
+    # Each surgery must be in exactly one room.
+    for surgery in data["surgeries"]:
+        model.AddExactlyOne(x[surgery, j] for j in data["rooms"])
 
-    # # The total duration in each room should be smaller and up to the average duration
-    # for j in data['rooms']:
-    #     model.Add(sum(x[(i, j)] * data['surgery_durations'][i] for i in data['surgeries']) <= data['avg_duration'])
-    
     def total_room_duration(room):
-        return sum([x[(surgery, room)] * data['surgery_durations'][surgery] for surgery in data['surgeries']])
+        return sum(
+            [
+                x[(surgery, room)] * data["surgery_durations"][surgery]
+                for surgery in data["surgeries"]
+            ]
+        )
 
-    for j in data['rooms']:
-        # model.Add(total_room_duration(j) >= data['min_total_duration'])  # Possibly not required
-        model.Add(total_room_duration(j) <= data['max_total_duration'])
+    for j in data["rooms"]:
+        model.Add(total_room_duration(j) <= data["max_total_duration"])
 
     def total_room_duration_differences():
-        total_room_durations = [total_room_duration(room) for room in data['rooms']]
+        total_room_durations = [total_room_duration(room) for room in data["rooms"]]
         all_perms = lazy_permute(total_room_durations)
         for idx, permutation in enumerate(all_perms):
-            temp = model.NewIntVar(-MAX_VAL_LIM, MAX_VAL_LIM, f"temp_{idx}")  # temporary variable, as a workaround for abs val
-            model.Add( temp == (permutation[0] - permutation[1]))
+            temp = model.NewIntVar(
+                -MAX_VAL_LIM, MAX_VAL_LIM, f"temp_{idx}"
+            )  # temporary variable, as a workaround for abs val
+            model.Add(temp == (permutation[0] - permutation[1]))
             model.AddAbsEquality(absolute_length_differences[idx], temp)
 
-
     total_room_duration_differences()
+    # End Constraints ---------------------------------------
 
+    # Optimization ------------------------------------------
     model.Minimize(sum(absolute_length_differences))
     solver = cp_model.CpSolver()
-    # solver.parameters.linearization_level = 0
-
-    # solver.parameters.enumerate_all_solutions = True
     status = solver.Solve(model)
-    print('Solve status: %s' % solver.StatusName(status))
-
 
     if status == cp_model.OPTIMAL:
         print(f"Total surgeries to schedule: {len(data['surgeries'])}")
         print(f"Optimal difference (lower is better): {solver.ObjectiveValue()}")
-        for j in data['rooms']:
-            assigned_surgeries = []
+        for operating_room_idx in data["rooms"]:
+            operating_room = rooms[operating_room_idx]
             total_duration = 0
-            for i in data['surgeries']:
-                if solver.Value(x[i, j]) > 0:
-                    assigned_surgeries.append(i)
-                    total_duration += data['surgery_durations'][i]
+            for surgery_idx in data["surgeries"]:
+                surgery = surgeries[surgery_idx]
+                if solver.Value(x[surgery_idx, operating_room_idx]) > 0:
+                    operating_room.surgeries_to_schedule.append(surgery)
+                    total_duration += surgery.duration
 
-            if assigned_surgeries:
-                print(f'Surgeries in OR{j}:', assigned_surgeries)
-                print('Daily Surgery Length:', total_duration)
-                print()
-        print()
-        print('Time = ', solver.WallTime(), ' milliseconds')
+            logger.debug(
+                f"Surgeries in {operating_room}: {operating_room.surgeries_to_schedule}"
+            )
+            logger.debug(
+                f"Total queue duration in {operating_room} is {total_duration} [m]"
+            )
+
+        logger.debug(f"Solver took {solver.WallTime():.2f} [ms] to finish")
     else:
-        print('The problem does not have an optimal solution.')
+        logger.warning(
+            f"The problem does not have an optimal solution.\n"
+            f"The solution status was deemed {solver.StatusName(status)}"
+        )
 
 
-if __name__ == '__main__':
-    main()
- 
+if __name__ == "__main__":
+    a = Surgery(name="a", duration_in_minutes=60, requirements=[])
+    b = Surgery(name="b", duration_in_minutes=60, requirements=["microscope"])
+    c = Surgery(name="c", duration_in_minutes=120, requirements=["microscope", "xray"])
+    # d = Surgery(name="d", duration_in_minutes=120, requirements=["microscope", "xray"])
+
+    o = OperatingRoom(id="OR1", properties=["microscope", "xray", "ct"])
+    p = OperatingRoom(id="OR2", properties=["microscope", "ct"])
+
+    or_list = [o, p]
+    surgery_list = [a, b, c]
+
+    distribute_surgeries_to_operating_rooms(surgery_list, or_list)
