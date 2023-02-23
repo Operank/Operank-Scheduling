9dc72408708e8600265a0b245217f58f7b7edd6c
Alon Budker <alonbudker@mail.tau.ac.il>
2023-02-23 20:03:39 +0200

    fix: aligned to black style formatting

--- src/operank_scheduling/algo/algo_helpers.py
+++ src/operank_scheduling/algo/algo_helpers.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 def intersection_size(l1: List[str], l2: List[str]):
     longest_list, shortest_list = (l1, l2) if len(l1) > len(l2) else (l2, l1)
     same_elements = 0
@@ -8,9 +9,10 @@ def intersection_size(l1: List[str], l2: List[str]):
             same_elements += 1
     return same_elements
 
+
 def lazy_permute(in_lst: List) -> List:
     out_lst = list()
     for i in range(len(in_lst)):
-        for j in range(i+1, len(in_lst)):
-            out_lst.append( (in_lst[i], in_lst[j]) )
+        for j in range(i + 1, len(in_lst)):
+            out_lst.append((in_lst[i], in_lst[j]))
     return out_lst
