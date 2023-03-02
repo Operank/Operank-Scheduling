ccb4bac395df2b254b45a13c9881f89eb369fc78
Alon Budker <alonio6@gmail.com>
2023-03-02 22:15:16 +0200

    fix: improved algo_helpers readability

--- src/operank_scheduling/algo/algo_helpers.py
+++ src/operank_scheduling/algo/algo_helpers.py
@@ -3,16 +3,18 @@ from typing import List
 
 def intersection_size(l1: List[str], l2: List[str]):
     longest_list, shortest_list = (l1, l2) if len(l1) > len(l2) else (l2, l1)
-    same_elements = 0
+    num_same_elements = 0
     for element in longest_list:
         if element in shortest_list:
-            same_elements += 1
-    return same_elements
+            num_same_elements += 1
+    return num_same_elements
 
 
 def lazy_permute(in_lst: List) -> List:
-    out_lst = list()
+    if len(in_lst) < 2:
+        return list()
+    permutations = list()
     for i in range(len(in_lst)):
         for j in range(i + 1, len(in_lst)):
-            out_lst.append((in_lst[i], in_lst[j]))
-    return out_lst
+            permutations.append((in_lst[i], in_lst[j]))
+    return permutations
