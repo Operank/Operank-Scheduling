83f4467f151b7ed2cef29fe8680110322e8e03be
Alon Budker <alonio6@gmail.com>
2023-03-02 22:57:29 +0200

    add: find project root directory easily

---
+++ src/operank_scheduling/models/io_utilities.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+
+def find_project_root() -> Path:
+    file_parts = list(Path(__file__).parts)
+    root_idx = file_parts.index("operank_scheduling")
+    return Path(*file_parts[:root_idx + 1])
