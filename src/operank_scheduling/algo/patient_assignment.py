e339ed9f66891baa2455161dbe58fd1a6897dad9
Alon Budker <alonio6@gmail.com>
2023-03-06 18:26:18 +0200

    add: get surgery by patient uuid

--- src/operank_scheduling/algo/patient_assignment.py
+++ src/operank_scheduling/algo/patient_assignment.py
@@ -1,12 +1,25 @@
 from typing import List
-from src.operank_scheduling.models.operank_models import Patient
+
+from src.operank_scheduling.models.operank_models import Patient, Surgery
 
 
 def sort_patients_by_priority(patient_list: List[Patient]) -> List[Patient]:
     return sorted(patient_list, key=lambda p: p.priority)
 
 
-def suggest_possible_dates(patients: List[Patient]):
+def get_surgery_by_patient(patient: Patient, surgeries: List[Surgery]) -> Surgery:
+    for surgery in surgeries:
+        if surgery.uuid == patient.uuid:
+            return surgery
+    raise ValueError("Failed to match surgery to patient with UUID")
+
+
+def suggest_feasible_dates(patient: Patient, surgeries: List[Surgery]) -> List:
+    procedure = get_surgery_by_patient(patient, surgeries)
+    pass
+
+
+def schedule_patients(patients: List[Patient], surgeries: List[Surgery]):
     """
     Sort patients by priority, and then for each patient:
         1. Find which surgery is required and it's duration, along with the required person, team or teams to perform
@@ -14,4 +27,8 @@ def suggest_possible_dates(patients: List[Patient]):
         3. Find ORs that are active in these days that also have an available timeslot
         4. Display these dates on the GUI (for now, just output)
     """
+    sorted_patients = sort_patients_by_priority(patients)
+    for patient in sorted_patients:
+        dates = suggest_feasible_dates(patient, surgeries)
+        print(dates)
     pass
