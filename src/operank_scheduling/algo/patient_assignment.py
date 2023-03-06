0ed44360926dd46b4e0c67f9ca6f1a9726ad9833
Alon Budker <alonio6@gmail.com>
2023-03-06 21:24:39 +0200

    add: load surgeons in the patient assignment phase

--- src/operank_scheduling/algo/patient_assignment.py
+++ src/operank_scheduling/algo/patient_assignment.py
@@ -1,6 +1,12 @@
 from typing import List
 
-from src.operank_scheduling.models.operank_models import Patient, Surgery
+from src.operank_scheduling.models.operank_models import (
+    OperatingRoom,
+    Patient,
+    Surgeon,
+    Surgery,
+    get_all_surgeons,
+)
 
 
 def sort_patients_by_priority(patient_list: List[Patient]) -> List[Patient]:
@@ -14,12 +20,19 @@ def get_surgery_by_patient(patient: Patient, surgeries: List[Surgery]) -> Surger
     raise ValueError("Failed to match surgery to patient with UUID")
 
 
-def suggest_feasible_dates(patient: Patient, surgeries: List[Surgery]) -> List:
+def suggest_feasible_dates(
+    patient: Patient,
+    surgeries: List[Surgery],
+    rooms: List[OperatingRoom],
+    surgeons: List[Surgeon],
+) -> List:
     procedure = get_surgery_by_patient(patient, surgeries)
-    pass
+    print(procedure)
 
 
-def schedule_patients(patients: List[Patient], surgeries: List[Surgery]):
+def schedule_patients(
+    patients: List[Patient], surgeries: List[Surgery], rooms: List[OperatingRoom]
+):
     """
     Sort patients by priority, and then for each patient:
         1. Find which surgery is required and it's duration, along with the required person, team or teams to perform
@@ -27,8 +40,9 @@ def schedule_patients(patients: List[Patient], surgeries: List[Surgery]):
         3. Find ORs that are active in these days that also have an available timeslot
         4. Display these dates on the GUI (for now, just output)
     """
+    surgeons = get_all_surgeons()
     sorted_patients = sort_patients_by_priority(patients)
     for patient in sorted_patients:
-        dates = suggest_feasible_dates(patient, surgeries)
+        dates = suggest_feasible_dates(patient, surgeries, rooms, surgeons)
         print(dates)
     pass
