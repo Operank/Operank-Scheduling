ec9a3862cce2f00668c138b75b0ac90b766e5441
Alon Budker <alonio6@gmail.com>
2023-03-06 18:34:21 +0200

    tests: used fixtures instead of manual params

--- tests/test_patient_assignment.py
+++ tests/test_patient_assignment.py
@@ -52,8 +52,9 @@ def test_patient_sorting():
         assert patient.priority == expected[idx]
 
 
-def test_surgery_to_patient_link():
-    p1 = Patient(
+@pytest.fixture
+def create_dummy_patients_and_surgeries():
+    patient = Patient(
         priority=1,
         name="a",
         patient_id="a",
@@ -63,22 +64,26 @@ def test_surgery_to_patient_link():
         uuid=1,
     )
     s1 = Surgery(name="a", duration_in_minutes=65, uuid=1)
-    surgeries = [s1]
-    assert get_surgery_by_patient(p1, surgeries) == s1
+    s2 = Surgery(name="a", duration_in_minutes=95, uuid=2)
+    surgeries = [s1, s2]
+    return (patient, surgeries)
 
 
-def test_surgery_to_patient_link_exception():
-    p1 = Patient(
+def test_surgery_to_patient_link(create_dummy_patients_and_surgeries):
+    patient, surgeries = create_dummy_patients_and_surgeries
+    assert get_surgery_by_patient(patient, surgeries) == surgeries[0]
+
+
+def test_surgery_to_patient_link_exception(create_dummy_patients_and_surgeries):
+    patient = Patient(
         priority=1,
         name="a",
         patient_id="a",
         surgery_name="a",
         referrer="a",
         estimated_duration_m=3,
-        uuid=1,
+        uuid=7,
     )
-    s1 = Surgery(name="a", duration_in_minutes=65, uuid=2)
-    s2 = Surgery(name="a", duration_in_minutes=95, uuid=3)
-    surgeries = [s1, s2]
+    _, surgeries = create_dummy_patients_and_surgeries
     with pytest.raises(ValueError):
-        get_surgery_by_patient(p1, surgeries) == s2
+        get_surgery_by_patient(patient, surgeries)
