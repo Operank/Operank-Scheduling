77b97ca10c6e898ed330a896b1e413eb84288e44
Alon Budker <alonio6@gmail.com>
2023-03-09 13:17:44 +0200

    add: added surgeon teams list as const
    Maybe consider revisiting this

--- src/operank_scheduling/models/enums.py
+++ src/operank_scheduling/models/enums.py
@@ -1,9 +1,11 @@
 weekdays_to_numbers = {
-    "Monday" : 0,
-    "Tuesday" : 1,
-    "Wednesday" : 2,
-    "Thursday" : 3,
-    "Friday" : 4,
-    "Saturday" : 4,
-    "Sunday" : 6
+    "Monday": 0,
+    "Tuesday": 1,
+    "Wednesday": 2,
+    "Thursday": 3,
+    "Friday": 4,
+    "Saturday": 4,
+    "Sunday": 6,
 }
+
+surgeon_teams = ["BARIATRIC", "ROBOTIC", "BREAST", "PROCTO"]
