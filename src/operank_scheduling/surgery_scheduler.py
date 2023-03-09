101da62d1445c544a96f0f925bbdcc0eda28456a
Alon Budker <alonio6@gmail.com>
2023-03-09 13:20:48 +0200

    temp: comment out irrelevant code for later removal

--- src/operank_scheduling/surgery_scheduler.py
+++ src/operank_scheduling/surgery_scheduler.py
@@ -1,12 +1,12 @@
-from typing import Dict, List
+# from typing import Dict, List
 
-from algo.algo_helpers import intersection_size
 from algo.distribution_models import (
     distribute_timeslots_to_days,
     distribute_timeslots_to_operating_rooms,
 )
-from loguru import logger
-from models.operank_models import OperatingRoom, Surgery, Timeslot
+
+# from loguru import logger
+from models.operank_models import OperatingRoom, Timeslot
 
 """
 # General Idea
@@ -19,73 +19,61 @@ from models.operank_models import OperatingRoom, Surgery, Timeslot
 """
 
 
-def find_suitable_rooms_for_surgery(
-    surgery: Surgery, list_of_rooms: List[OperatingRoom]
-) -> List:
-    assert surgery.requirements
-    suitable_rooms = []
-    for room in list_of_rooms:
-        match_strength = intersection_size(surgery.requirements, room.properties)
-        if match_strength >= len(surgery.requirements):
-            suitable_rooms.append(room)
-    return suitable_rooms
+# def find_suitable_rooms_for_special_surgeries(
+#     list_of_surgeries: List[Surgery], list_of_rooms: List[OperatingRoom]
+# ) -> Dict[Surgery, List[OperatingRoom]]:
+#     special_surgery_mapping = dict()
+#     for surgery in list_of_surgeries:
+#         special_surgery_mapping[surgery] = find_suitable_rooms_for_surgery(
+#             surgery, list_of_rooms
+#         )
+#     return special_surgery_mapping
 
 
-def find_suitable_rooms_for_special_surgeries(
-    list_of_surgeries: List[Surgery], list_of_rooms: List[OperatingRoom]
-) -> Dict[Surgery, List[OperatingRoom]]:
-    special_surgery_mapping = dict()
-    for surgery in list_of_surgeries:
-        special_surgery_mapping[surgery] = find_suitable_rooms_for_surgery(
-            surgery, list_of_rooms
-        )
-    return special_surgery_mapping
+# def build_remaining_surgery_mapping(
+#     surgery_list: List[Surgery],
+#     mapping: Dict[Surgery, List[OperatingRoom]],
+#     list_of_rooms: List[OperatingRoom],
+# ):
+#     remainder_mapping = dict()
+#     surgeries_without_special_requirements = [
+#         surgery for surgery in surgery_list if not surgery.requirements
+#     ]
+#     for surgery in surgeries_without_special_requirements:
+#         remainder_mapping[surgery] = list_of_rooms
+#     remainder_mapping.update(mapping)
+#     return remainder_mapping
 
 
-def build_remaining_surgery_mapping(
-    surgery_list: List[Surgery],
-    mapping: Dict[Surgery, List[OperatingRoom]],
-    list_of_rooms: List[OperatingRoom],
-):
-    remainder_mapping = dict()
-    surgeries_without_special_requirements = [
-        surgery for surgery in surgery_list if not surgery.requirements
-    ]
-    for surgery in surgeries_without_special_requirements:
-        remainder_mapping[surgery] = list_of_rooms
-    remainder_mapping.update(mapping)
-    return remainder_mapping
+# def _only_one_available_option(surgery: Surgery, possible_rooms: dict):
+#     return len(possible_rooms[surgery]) == 1
 
 
-def _only_one_available_option(surgery: Surgery, possible_rooms: dict):
-    return len(possible_rooms[surgery]) == 1
+# def assign_special_surgeries(
+#     surgery_list: List[Surgery], list_of_rooms: List[OperatingRoom]
+# ) -> Dict[Surgery, List[OperatingRoom]]:
+#     logger.info(
+#         f"[Assignment] Assigning {len(surgery_list)} surgeries to operating rooms"
+#     )
+#     surgeries_with_special_requirements = [
+#         surgery for surgery in surgery_list if surgery.requirements
+#     ]
+#     mapping = find_suitable_rooms_for_special_surgeries(
+#         surgeries_with_special_requirements, list_of_rooms
+#     )
+#     scheduled_surgeries = []
+#     for surgery in mapping:
+#         if _only_one_available_option(surgery, mapping):
+#             mapping[surgery][0].surgeries_to_schedule.append(surgery)
+#             logger.debug(
+#                 f"[1:1] Assigned surgery {surgery} to room {mapping[surgery][0]}"
+#             )
+#             scheduled_surgeries.append(surgery)
 
+#     [mapping.pop(surgery) for surgery in scheduled_surgeries]
 
-def assign_special_surgeries(
-    surgery_list: List[Surgery], list_of_rooms: List[OperatingRoom]
-) -> Dict[Surgery, List[OperatingRoom]]:
-    logger.info(
-        f"[Assignment] Assigning {len(surgery_list)} surgeries to operating rooms"
-    )
-    surgeries_with_special_requirements = [
-        surgery for surgery in surgery_list if surgery.requirements
-    ]
-    mapping = find_suitable_rooms_for_special_surgeries(
-        surgeries_with_special_requirements, list_of_rooms
-    )
-    scheduled_surgeries = []
-    for surgery in mapping:
-        if _only_one_available_option(surgery, mapping):
-            mapping[surgery][0].surgeries_to_schedule.append(surgery)
-            logger.debug(
-                f"[1:1] Assigned surgery {surgery} to room {mapping[surgery][0]}"
-            )
-            scheduled_surgeries.append(surgery)
-
-    [mapping.pop(surgery) for surgery in scheduled_surgeries]
-
-    mapping = build_remaining_surgery_mapping(surgery_list, mapping, list_of_rooms)
-    return mapping
+#     mapping = build_remaining_surgery_mapping(surgery_list, mapping, list_of_rooms)
+#     return mapping
 
 
 if __name__ == "__main__":
