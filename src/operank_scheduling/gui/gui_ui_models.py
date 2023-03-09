c29aac7fa8d4602c1eb50fc6d0dfbaae11aeb6bf
Alon Budker <alonio6@gmail.com>
2023-03-09 20:16:13 +0200

    add: gui is now dynamic

--- src/operank_scheduling/gui/gui_ui_models.py
+++ src/operank_scheduling/gui/gui_ui_models.py
@@ -1,42 +1,101 @@
 from nicegui import ui
-from typing import List
+from typing import List, Callable
 from operank_scheduling.models.operank_models import Patient
 import datetime
 
 
 class PatientSchedulingScreen:
-    def __init__(self, patient: Patient, suggested_dates: List[datetime.datetime]) -> None:
-        with ui.column().classes('m-auto'):
-            with ui.card().classes('m-auto'):
-                ui.label(patient.name)
-                ui.label(patient.patient_id)
-                ui.label(patient.surgery_name)
+    def __init__(
+        self, patient: Patient, suggested_dates: List[datetime.datetime]
+    ) -> None:
+        with ui.card().classes("m-auto"):
+            with ui.card().classes("m-auto"):
+                FormattedTextRow(
+                    title="Patient:", text=patient.name, icon="personal_injury"
+                )
+                FormattedTextRow(title="ID:", text=patient.patient_id, icon="badge")
+                FormattedTextRow(
+                    title="Procedure:",
+                    text=patient.surgery_name,
+                    icon="health_and_safety",
+                )
             DateSelectionOptionsRow(suggested_dates)
 
 
 class DateSelectionOptionsRow:
     def __init__(self, list_of_dates: List[datetime.datetime]) -> None:
         with ui.row():
-            for date in list_of_dates:
-                DateSelectionCard(date)
+            for idx, date in enumerate(list_of_dates):
+                DateSelectionCard(date, option_index=idx)
 
 
 class DateSelectionCard(ui.card):
-    def __init__(self, date_and_time: datetime.datetime):
+    def __init__(self, date_and_time: datetime.datetime, option_index: int):
         super().__init__()
         self.displayed_date = datetime.datetime.strftime(
             date_and_time, "%d/%m/%Y @ %H:%M"
         )
+        self.option_index = option_index
         with self:
-            with ui.column().classes('items-center'):
+            with ui.column().classes("items-center"):
                 ui.label(self.displayed_date)
-                ui.button("Select")
+                ui.button("Select", on_click=self.button_cb)
 
-        self.on("dragover.prevent", self.highlight)
-        self.on("dragleave", self.unhighlight)
+    def button_cb(self):
+        ui.notify(self.option_index, closeBtn=True)
+        return self.option_index
 
-    def highlight(self) -> None:
-        self.classes(add="bg-gray-400")
 
-    def unhighlight(self) -> None:
-        self.classes(remove="bg-gray-400")
+class FormattedTextRow:
+    def __init__(self, title: str, text: str, icon: str = "") -> None:
+        with ui.row().classes("items-center"):
+            if icon != "":
+                ui.icon(icon)
+            ui.label(title).classes("text-weight-bold")
+            ui.label(text).classes("text-weight-regular")
+
+
+class PatientNavigationControls:
+    def __init__(self, state_func: Callable) -> None:
+        self.state_func = state_func
+        with ui.row():
+            ui.button(text="Previous Patient", on_click=self.prev_cb)
+            ui.button(text="Next Patient", on_click=self.next_cb)
+
+    def next_cb(self):
+        ui.notify("Moving to next patient", closeBtn=True)
+        self.state_func("up")
+
+    def prev_cb(self):
+        ui.notify("Moving to previous patient", closeBtn=True)
+        self.state_func("down")
+
+
+class PatientSchedulingUI:
+    def __init__(self, patient_list: List[Patient], datetimes_list) -> None:
+        self.content = ui.column()
+        self.current_patient_idx = 0
+        self.patient_list = patient_list
+        self.datetimes_list = datetimes_list
+        self.display_patient_scheduling_ui()
+
+    def draw_inner_ui(self, patient, datetimes_list):
+        with self.content.classes("items-center"):
+            self.patient_state = PatientSchedulingScreen(patient, datetimes_list)
+            PatientNavigationControls(self.update_app_state)
+
+    def display_patient_scheduling_ui(self):
+        self.content.clear()
+        self.draw_inner_ui(
+            self.patient_list[self.current_patient_idx], self.datetimes_list
+        )
+
+    def update_app_state(self, direction: str):
+        self.content.clear()
+        if direction == "up":
+            self.current_patient_idx += 1
+        else:
+            self.current_patient_idx -= 1
+        self.draw_inner_ui(
+            self.patient_list[self.current_patient_idx], self.datetimes_list
+        )
--- src/operank_scheduling/run_gui.py
+++ src/operank_scheduling/run_gui.py
@@ -1,8 +1,11 @@
 from nicegui import ui
 import datetime
-from gui.gui_ui_models import PatientSchedulingScreen
+from gui.gui_ui_models import PatientSchedulingUI
 from operank_scheduling.models.operank_models import get_all_surgeons
-from operank_scheduling.models.parse_data_to_models import load_operating_rooms_from_json, load_patients_from_json
+from operank_scheduling.models.parse_data_to_models import (
+    load_operating_rooms_from_json,
+    load_patients_from_json,
+)
 from operank_scheduling.models.io_utilities import find_project_root
 
 datetimes_list = [
@@ -13,14 +16,13 @@ datetimes_list = [
 
 assets_dir = find_project_root() / "assets"
 patient_list, surgery_list, timeslot_list = load_patients_from_json(
-        assets_dir / "example_patient_data.json"
+    assets_dir / "example_patient_data.json"
 )
 operating_rooms = load_operating_rooms_from_json(
-        assets_dir / "example_operating_room_schedule.json"
+    assets_dir / "example_operating_room_schedule.json"
 )
 surgeons = get_all_surgeons()
-PatientSchedulingScreen(patient_list[0], datetimes_list)
 
-ui.run(
-    title="Operank",
-)
+ui.run(title="Operank", favicon=str(assets_dir / "operank_favicon.jpg"))
+
+PatientSchedulingUI(patient_list, datetimes_list)
