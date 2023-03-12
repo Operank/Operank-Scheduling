f1f6f056f671aabba75ade8aef60089f01600fc1
Alon Budker <alonio6@gmail.com>
2023-03-12 21:53:26 +0200

    add: logo to header

--- src/operank_scheduling/gui/static_ui_elements.py
+++ src/operank_scheduling/gui/static_ui_elements.py
@@ -5,6 +5,7 @@ from operank_scheduling.models.io_utilities import find_project_root
 class OperankHeader:
     def __init__(self) -> None:
         app.add_static_files('/images', str(find_project_root() / "assets"))
-        self.header = ui.header()
-        with self.header.classes():
+        self.header = ui.header().classes('justify-between')
+        with self.header:
             ui.label("Operank")
+            ui.image("/images/operank_logo.jpg").style("height: 40px; width: 40px")
