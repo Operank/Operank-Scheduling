29bd84b15c570eea88f831e5eacf680007e2d621
Alon Budker <alonio6@gmail.com>
2023-03-12 22:01:02 +0200

    refactor: switch to double-quotes (black)

--- src/operank_scheduling/gui/static_ui_elements.py
+++ src/operank_scheduling/gui/static_ui_elements.py
@@ -4,8 +4,8 @@ from operank_scheduling.models.io_utilities import find_project_root
 
 class OperankHeader:
     def __init__(self) -> None:
-        app.add_static_files('/images', str(find_project_root() / "assets"))
-        self.header = ui.header().classes('justify-between')
+        app.add_static_files("/images", str(find_project_root() / "assets"))
+        self.header = ui.header().classes("justify-between")
         with self.header:
             ui.label("Operank")
             ui.image("/images/operank_logo.jpg").style("height: 40px; width: 40px")
