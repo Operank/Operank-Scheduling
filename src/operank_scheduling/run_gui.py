5e96dddc4aa18e726fda0400b950afc50c1531c2
Alon Budker <alonio6@gmail.com>
2023-04-18 18:23:10 +0300

    add: load surgeon data on gui startup + logs

--- src/operank_scheduling/run_gui.py
+++ src/operank_scheduling/run_gui.py
@@ -1,6 +1,7 @@
 import os
 from nicegui import ui
 import datetime
+from loguru import logger
 from gui.gui_ui_models import StateManager, AppState
 from gui.static_ui_elements import OperankHeader
 from operank_scheduling.models.operank_models import get_all_surgeons
@@ -13,19 +14,25 @@ from operank_scheduling.algo.surgery_distribution_models import (
     perform_preliminary_scheduling,
 )
 from operank_scheduling.algo.patient_assignment import sort_patients_by_priority
-
+from operank_scheduling.models.parse_hopital_data import load_surgeon_schedules
 
 assets_dir = find_project_root() / "assets"
 os.environ["MATPLOTLIB"] = "false"
 
+logger.info("Loading patient data...")
 patient_list, surgery_list, timeslot_list = load_patients_from_json(
     assets_dir / "example_patient_data.json"
 )
+logger.info("Loading OR data...")
 operating_rooms = load_operating_rooms_from_json(
     assets_dir / "example_operating_room_schedule.json"
 )
+logger.info("Sorting patients by priority...")
 patient_list = sort_patients_by_priority(patient_list)
+logger.info("Loading surgeon data...")
 surgeons = get_all_surgeons()
+logger.info("Loading surgeon schedules...")
+load_surgeon_schedules(surgeons)
 operating_rooms = operating_rooms[:2]
 perform_preliminary_scheduling(timeslot_list, operating_rooms)
 
